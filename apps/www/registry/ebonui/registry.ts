e8b7b4f7c62f882b01f75b305d1ed9108853aa4b
Nnamdi Iwara <naijarace1@gmail.com>
2026-01-22 11:04:49 +0100

    fix: error in installation of components

--- apps/www/registry/ebonui/registry.ts
+++ apps/www/registry/ebonui/registry.ts
@@ -31,7 +31,7 @@ const NEW_YORK_V4_STYLE = {
 
 export const registry = {
   name: "ebonui",
-  homepage: "https://ui.shadcn.com",
+  homepage: "https://ebonui.com",
   items: z.array(registryItemSchema).parse(
     [
       {
